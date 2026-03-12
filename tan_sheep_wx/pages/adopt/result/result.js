4316272aca238ceea1b09771f784a5e864a8abf8
youyouzz <13247221+suyoda@user.noreply.gitee.com>
2026-03-12 17:25:26 +0800

    feat: 小程序端，优化领养结果页面，增加分页功能和跳转页码输入

--- tan_sheep_wx/pages/adopt/result/result.js
+++ tan_sheep_wx/pages/adopt/result/result.js
@@ -4,9 +4,14 @@ const API = require('../../../utils/api.js');
 Page({
     data: {
         sheepList: [],
-        loading: false
+        pagedSheepList: [],
+        loading: false,
+        currentPage: 1,
+        pageSize: 10,
+        totalPages: 1,
+        jumpPageInput: ''
     },
-    
+
     onLoad: function(options) {
         // 从页面参数中获取搜索结果
         if (options.data) {
@@ -19,17 +24,115 @@ Page({
                     }
                 });
                 this.setData({
-                    sheepList: sheepList || []
+                    sheepList: sheepList || [],
+                    currentPage: 1,
+                    jumpPageInput: ''
                 });
+                this.updatePagedList();
             } catch (e) {
                 console.error('解析搜索结果失败', e);
                 this.setData({
-                    sheepList: []
+                    sheepList: [],
+                    pagedSheepList: [],
+                    currentPage: 1,
+                    totalPages: 1,
+                    jumpPageInput: ''
                 });
             }
         }
     },
-    
+
+    // 更新当前页数据
+    updatePagedList: function () {
+        var list = this.data.sheepList || [];
+        var pageSize = this.data.pageSize || 10;
+        var totalPages = Math.max(1, Math.ceil(list.length / pageSize));
+        var currentPage = this.data.currentPage;
+
+        if (currentPage > totalPages) {
+            currentPage = totalPages;
+        }
+        if (currentPage < 1) {
+            currentPage = 1;
+        }
+
+        var start = (currentPage - 1) * pageSize;
+        var end = start + pageSize;
+
+        this.setData({
+            totalPages: totalPages,
+            currentPage: currentPage,
+            pagedSheepList: list.slice(start, end)
+        });
+    },
+
+    // 首页
+    goFirstPage: function () {
+        if (this.data.currentPage <= 1) return;
+        this.setData({
+            currentPage: 1
+        });
+        this.updatePagedList();
+    },
+
+    // 上一页
+    goPrevPage: function () {
+        if (this.data.currentPage <= 1) return;
+        this.setData({
+            currentPage: this.data.currentPage - 1
+        });
+        this.updatePagedList();
+    },
+
+    // 下一页
+    goNextPage: function () {
+        if (this.data.currentPage >= this.data.totalPages) return;
+        this.setData({
+            currentPage: this.data.currentPage + 1
+        });
+        this.updatePagedList();
+    },
+
+    // 末页
+    goLastPage: function () {
+        if (this.data.currentPage >= this.data.totalPages) return;
+        this.setData({
+            currentPage: this.data.totalPages
+        });
+        this.updatePagedList();
+    },
+
+    // 输入跳转页码
+    onJumpInput: function (e) {
+        this.setData({
+            jumpPageInput: e.detail.value
+        });
+    },
+
+    // 跳转到指定页
+    jumpToPage: function () {
+        var input = this.data.jumpPageInput;
+        var target = parseInt(input, 10);
+        var totalPages = this.data.totalPages || 1;
+
+        if (!target || isNaN(target)) {
+            wx.showToast({
+                title: '请输入页码',
+                icon: 'none'
+            });
+            return;
+        }
+
+        if (target < 1) target = 1;
+        if (target > totalPages) target = totalPages;
+
+        this.setData({
+            currentPage: target,
+            jumpPageInput: ''
+        });
+        this.updatePagedList();
+    },
+
     viewSheepDetail: function(e) {
         var sheepId = e.currentTarget.dataset.id;
         wx.navigateTo({
